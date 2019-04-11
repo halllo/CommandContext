635ece1e121567b6acee4799423485c293434f39
Manuel Naujoks <mnaujoks@gmail.com>
2019-04-11 19:59:11 +0200

    started .net core 3 support

---
+++ CommandContext/CommandContext.Sample.NetCore/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+
+namespace CommandContext.Sample.NetCore
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var viewModel = this.DataContext = new ViewModel
+			{
+				Input = "World",
+				SubViewModel1 = new ViewModel { Input = "item1" },
+				SubViewModel2 = new ViewModel { Input = "item2" },
+			};
+			this.CommandContext(new Controller { ViewModel = (ViewModel)viewModel });
+		}
+	}
+
+
+	public class Controller
+	{
+		public ViewModel ViewModel { get; set; }
+
+		public bool CanSayHi() => ViewModel.Enabled;
+		public void SayHi() => MessageBox.Show($"Hi!");
+
+		public void SayHiTo(object name) => MessageBox.Show($"Hi '{name}'!");
+	}
+
+
+	public class ViewModel
+	{
+		public bool Enabled { get; set; }
+		public string Input { get; set; }
+
+		public ViewModel SubViewModel1 { get; set; }
+		public ViewModel SubViewModel2 { get; set; }
+	}
+}
