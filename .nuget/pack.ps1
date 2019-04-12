8a9cf64949591d3898adb9bc00abcd2672b9c8d1
Manuel Naujoks <mnaujoks@gmail.com>
2019-04-12 16:58:05 +0200

    ci for netcore3

--- .nuget/pack.ps1
+++ .nuget/pack.ps1
@@ -1,5 +1,5 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$version = [System.Reflection.Assembly]::LoadFile("$root\CommandContext\CommandContext\bin\Release\CommandContext.dll").GetName().Version
+$version = [System.Reflection.Assembly]::LoadFile("$root\CommandContext\CommandContext.NetFramework\bin\Release\CommandContext.NetFramework.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
